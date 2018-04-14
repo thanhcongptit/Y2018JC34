44c64ab846d72583294c8c191657ca6a19717d6e
Administrator <Administrator@haidono>
2018-04-15 01:17:47 +0700

    chinh sua noi dung

---
+++ Y2018JC34/src/com/imic/students/DO_NAM_HAI/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.imic.students.DO_NAM_HAI;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello student of Y2018JC34 class!");
+	}
+
+}
