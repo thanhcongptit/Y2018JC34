e31e4bb0dc7671e4655a02082f55325403a624ef
Administrator <Administrator@haidono>
2018-04-17 19:57:57 +0700

    DO NAM HAI bai tap 1

---
+++ Y2018JC34/src/com/imic/students/DO_NAM_HAI/TenTuoi.java
@@ -0,0 +1,13 @@
+package com.imic.students.DO_NAM_HAI;
+
+public class TenTuoi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String HoTen = "Do Nam Hai";
+		int tuoi = 27;
+		System.out.println("Ho va ten " + HoTen +" "+ tuoi +" tuoi ");
+	
+	}
+
+}
