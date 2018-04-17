93331a349cb37db5a9608e949666e5ae41a6f71d
hb2 <hb2@DESKTOP-39KL164>
2018-04-17 20:03:23 +0700

    a

--- Y2018JC34/src/com/imic/students/duong/thuchanh1.java
+++ Y2018JC34/src/com/imic/students/duong/thuchanh1.java
@@ -3,11 +3,12 @@ package com.imic.students.duong;
 public class thuchanh1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-String ten = "Vu Viet Duong";
-int tuoi = 21;
-System.out.println("ten" + ten);
-System.out.println("tuoi" + tuoi);
+		
+		String 	ten = "Vu Viet Duong";
+		int 	tuoi = 21;
+		System.out.println("ten: " + ten);
+		System.out.println("tuoi: " + tuoi);
+		
 
 	}
 
