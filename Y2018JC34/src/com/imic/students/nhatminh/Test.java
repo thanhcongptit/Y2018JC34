7c9bfd25321e8244420ffeed737470ae831cadfd
PC <PC@DESKTOP-IO7V3US>
2018-04-17 20:10:48 +0700

    fffff Signed-off-by: PC <PC@DESKTOP-IO7V3US>

---
+++ Y2018JC34/src/com/imic/students/nhatminh/Test.java
@@ -0,0 +1,15 @@
+package com.imic.students.nhatminh;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int age = 10;
+        String name ="Pham Hoang Nhat Minh";
+        System.out.println("Ho va ten: "+name); 
+        System.out.println("Tuoi"+age);
+        
+        		
+	}
+
+}
