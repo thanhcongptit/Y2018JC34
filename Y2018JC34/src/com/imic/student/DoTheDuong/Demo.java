e14371e14a9c64640d76ac8fa70334398b0b7ee4
Administrator <Administrator@PC>
2018-04-17 22:29:13 +0700

    Nhap ho ten, tuoi, dia chi va in ra man hinh

--- Y2018JC34/src/com/imic/student/DoTheDuong/Demo.java
+++ Y2018JC34/src/com/imic/student/DoTheDuong/Demo.java
@@ -1,7 +1,22 @@
 package com.imic.student.DoTheDuong;
 
+import java.util.Scanner;
+
 public class Demo {
 	public static void main(String args[]){
+		Scanner scanner = new Scanner(System.in);
 		
+		System.out.println("Nhap ho ten:");
+		String hoTen = scanner.nextLine();
+		System.out.println("Ho va Ten: " + hoTen);
+		
+		System.out.println("Nhap tuoi:");
+		int tuoi = scanner.nextInt();
+		System.out.println("Tuoi: " + tuoi);
+		scanner.nextLine();
+		
+		System.out.println("Nhap dia chi:");
+		String diaChi = scanner.nextLine();
+		System.out.println("Dia chi: " + diaChi);
 	}
 }
