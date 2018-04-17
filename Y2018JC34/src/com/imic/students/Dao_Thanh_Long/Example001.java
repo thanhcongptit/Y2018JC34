5e9f05a8ce6484e1db5b8effc766d4c6336f5688
DaoThanhLong <30427088+DaoThanhLong@users.noreply.github.com>
2018-04-17 20:06:31 +0700

    jjj

--- Y2018JC34/src/com/imic/students/Dao_Thanh_Long/Example001.java
+++ Y2018JC34/src/com/imic/students/Dao_Thanh_Long/Example001.java
@@ -3,7 +3,7 @@ package com.imic.students.Dao_Thanh_Long;
 public class Example001 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stubkkkk
 		String name = "Dao Thanh Long";
 		int age	= 25;
 		System.out.println("My full name : " + name + " - Age : " + age);
