877bdd250fcd8846d77bfab04521df0db7e5d837
hb2 <hb2@DESKTOP-39KL164>
2018-04-17 20:51:46 +0700

    a

--- Y2018JC34/src/com/imic/students/duong/thuchanh1.java
+++ Y2018JC34/src/com/imic/students/duong/thuchanh1.java
@@ -1,14 +1,20 @@
 package com.imic.students.duong;
 
+import java.util.Scanner;
+
 public class thuchanh1 {
 
 	public static void main(String[] args) {
-		
-		String 	ten = "Vu Viet Duong";
-		int 	tuoi = 21;
-		System.out.println("ten: " + ten);
-		System.out.println("tuoi: " + tuoi);
-		
+
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("moi ban nhap so tuoi");
+		int tuoi = scanner.nextInt();
+		scanner.nextLine();
+		System.out.println("gia tri tuoi " + tuoi);
+		System.out.println("Moi ban nhap dia chi");
+		String diaChi = scanner.nextLine();
+		scanner.nextLine();
+		System.out.println("dia chi " + diaChi);
 
 	}
 
