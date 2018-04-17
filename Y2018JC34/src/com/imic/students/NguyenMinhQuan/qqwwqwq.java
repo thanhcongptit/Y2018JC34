98def579147a85bd90b5ce49245cd212bd1470b2
anhgaihoi2000 <quannmph04929@fpt.edu.vn>
2018-04-17 20:25:47 +0700

    bt

---
+++ Y2018JC34/src/com/imic/students/NguyenMinhQuan/qqwwqwq.java
@@ -0,0 +1,15 @@
+package com.imic.students.NguyenMinhQuan;
+
+public class qqwwqwq {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 21 ;
+		String ten = "Nguyen Minh Quan";
+		System.out.println("Ten toi la" +ten);
+		System.out.println("Tuoi toi la" +a);
+		
+
+	}
+
+}
