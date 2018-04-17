a0cc152384464e54040f61245ef0fc7764521d6c
Administrator <Administrator@haidono>
2018-04-17 20:49:19 +0700

    bai tap nhap ho ten

---
+++ Y2018JC34/src/com/imic/students/DO_NAM_HAI/HoVaTen.java
@@ -0,0 +1,23 @@
+package com.imic.students.DO_NAM_HAI;
+
+import java.util.Scanner;
+
+
+public class HoVaTen {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner nhap = new Scanner(System.in);
+		System.out.print("ho va ten : ");
+		String hoten = nhap.nextLine();
+		
+		
+		
+			
+		System.out.print("tuoi : ");
+		int tuoi = nhap.nextInt();
+		System.out.println("ho va ten " + hoten + " " + tuoi + " tuoi ");
+
+	}
+
+}
