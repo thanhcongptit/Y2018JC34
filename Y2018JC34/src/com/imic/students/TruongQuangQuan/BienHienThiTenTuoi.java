8e1b11629b51eb80666bdd5f2dbce9e8b2c0ef7a
Icream1405 <Icream1405@Icream1405>
2018-04-17 19:52:36 +0700

    Hien Thi Ten Tuoi

---
+++ Y2018JC34/src/com/imic/students/TruongQuangQuan/BienHienThiTenTuoi.java
@@ -0,0 +1,11 @@
+package com.imic.students.TruongQuangQuan;
+
+public class BienHienThiTenTuoi {
+	public static void main(String[] args) {
+		String name = "Truong Quang Quan";
+		int age = 28;
+		System.out.println("Ten : "+name);
+		System.out.println("Tuoi : "+age);
+	}
+
+}
