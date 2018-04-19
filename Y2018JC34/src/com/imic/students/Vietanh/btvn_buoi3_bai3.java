debd9e4011330c3d904b9de8ae51c3ad92f782a7
DELL <DELL@DESKTOP-3QRE1GP>
2018-04-19 20:22:09 +0700

    Viet anh: btvn buoi 3

---
+++ Y2018JC34/src/com/imic/students/Vietanh/btvn_buoi3_bai3.java
@@ -0,0 +1,16 @@
+package com.imic.students.Vietanh;
+
+import java.util.Scanner;
+
+public class btvn_buoi3_bai3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Xin moi nhap canh cua khoi lap phuong");
+		int canh = sc.nextInt();
+		
+		System.out.println("The tich la: "+(canh*canh*canh));
+
+	}
+
+}
