766cb0b4194fa87d43e3872187bde447e79d2019
hb2 <hb2@DESKTOP-39KL164>
2018-04-20 08:52:22 +0700

    a

---
+++ Y2018JC34/src/com/imic/students/duong/Lab1bai2.java
@@ -0,0 +1,13 @@
+package com.imic.students.duong;
+
+import java.util.Scanner;
+
+public class Lab1bai2 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+
+	}
+
+}
