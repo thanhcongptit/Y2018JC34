895415a72f1da51065bb73c7cd6f4826cd3d81f9
hb2 <hb2@DESKTOP-39KL164>
2018-04-20 09:38:58 +0700

    a

--- Y2018JC34/src/com/imic/students/duong/Lab1bai2.java
+++ Y2018JC34/src/com/imic/students/duong/Lab1bai2.java
@@ -6,7 +6,15 @@ public class Lab1bai2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
+		System.out.println("nhap chieu dai = ");
+		double dai = scanner.nextInt();
+		System.out.println("nhap chieu rong = ");
+		double rong = scanner.nextInt();
+		double chuvi=(dai+rong)*2;
+		double dientich=dai*rong;
+		System.out.println("chu vi = " +chuvi);
+		System.out.println("dien tich = "+dientich);
+		System.out.println("canh nho la: " +Math.min(rong, dai));
 
 	}
 
---
+++ Y2018JC34/src/com/imic/students/duong/Lab1bai3.java
@@ -0,0 +1,10 @@
+package com.imic.students.duong;
+
+public final class Lab1bai3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
