ee8baf1e21a1c585452a8791f9bdf42a96e89878
Administrator <Administrator@PC.net.fpt>
2018-04-23 23:13:04 +0700

    Bạn muốn hẹn hò

--- Y2018JC34/src/com/imic/student/DoTheDuong/Baitap2.java
+++ Y2018JC34/src/com/imic/student/DoTheDuong/Baitap2.java
@@ -8,37 +8,37 @@ public class Baitap2 {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Thong tin chuong trinh Ban Muon Hen Ho");
+		System.out.println("Thông tin chươngg trình Bạn Muốn Hẹn Hò");
 		System.out.println("Nhập Họ và Tên");
 		String hoTen = scanner.nextLine();
 		if (hoTen.equals("")) {
-			System.out.println("Ban chua nhap ho ten");
+			System.out.println("Bạn chưa nhập họ tên");
 		}
 		
 		System.out.println("Nhập tuổi");
 		String tuoi = scanner.nextLine();
 		if (tuoi.equals("")) {
-			System.out.println("Ban chua nhap tuoi");
+			System.out.println("Bạn chưa nhập tuổi");
 		}
 		
 		System.out.println("Nhập giới tính");
 		String gioiTinh = scanner.nextLine();
 		if (gioiTinh.equals("")) {
-			System.out.println("Ban chua nhap gioi tinh");
+			System.out.println("Bạn chưa nhập giới tính");
 		}
 
 		System.out.println("Tình trạng hôn nhân");
 		String honNhan = scanner.nextLine();
 		if (honNhan.equals("")) {
-			System.out.println("Ban chua nhap tinh trang hon nhan");
+			System.out.println("Bạn chưa nhập Tình trạng hôn nhân");
 		}
 			
-			if (Integer.parseInt(tuoi) >= 22 && gioiTinh.equals("Nam") && honNhan.equals("docthan")) {
+			if (Integer.parseInt(tuoi) >= 22 && gioiTinh.equals("Nam") && honNhan.equals("Độc thân") || honNhan.equals("Ly hôn")) {
 				System.out.println("Bạn được tham gia");
-			} else if (gioiTinh.equals("Nu") && Integer.parseInt(tuoi) >= 18 && honNhan.equals("Docthan")) {
-				System.out.println("Ban du dieu kien tham gia");
+			} else if (gioiTinh.equals("Nữ") && Integer.parseInt(tuoi) >= 18 && honNhan.equals("Độc thân") || honNhan.equals("Ly hôn")) {
+				System.out.println("Bạn đủ điều kiện tham gia");
 			} else {
-				System.out.println("Ban chua du dieu kien tham gia");
+				System.out.println("Bạn chưa đủ điều kiện tham gia");
 			}
 		} 
 	}
