b74a1a71e21a56b6bf1511fdecc369f9f17bc954
Duy Diệu <Duy Diu@DESKTOP-THNIB20>
2018-04-23 14:45:16 +0700

    phạm duy diệu

--- Y2018JC34/src/com/imic/students/PhamDuyDieu/BaiTapVeNha/BuoiThu4/Bai6_ChonKieuChanLe.java
+++ Y2018JC34/src/com/imic/students/PhamDuyDieu/BaiTapVeNha/BuoiThu4/Bai6_ChonKieuChanLe.java
@@ -1,10 +1,35 @@
 package com.imic.students.PhamDuyDieu.BaiTapVeNha.BuoiThu4;
 
+import java.util.Scanner;
+
 public class Bai6_ChonKieuChanLe {
-
+	/*
+	 * Yêu cầu: Nhập vào 1 số cần xử lý và chọn kiểu chẵn hoặc lẻ. Duyệt từ 0 đến số
+	 * đó xem có bao nhiêu số chắn hoặc lẻ. Sử dụng While - Do While - For
+	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int soCanXuLy;
+		String chonKieu;
+		String chuoiSo = "";
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhập số cần xử lý: ");
+		soCanXuLy = sc.nextInt();
+		if (soCanXuLy % 2 == 0) {
+			System.out.println("Số trên là số chẵn.\nTừ 0 đến " + soCanXuLy + " có " + (soCanXuLy / 2) + " số chẵn.");
+			for (int i = 0; i <= soCanXuLy; i++) {
+				if (i % 2 == 0) {
+					chuoiSo = chuoiSo + i + " ";
+				}
+			}
+		} else {
+			System.out.println("Số trên là số lẻ.\nTừ 0 đến " + soCanXuLy + " có " + ((soCanXuLy / 2) + 1) + " số lẻ.");
+			for (int i = 0; i <= soCanXuLy; i++) {
+				if (i % 2 != 0) {
+					chuoiSo = chuoiSo + i + " ";
+				}
 
+			}
+		}
+		System.out.println("Chuỗi số đó là: \n"+ chuoiSo);
 	}
-
 }
