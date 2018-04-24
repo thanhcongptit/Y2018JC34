b599822bd561b661859780ee286be0b4b1e75454
TuyenThanh <TuyenThanh@TuyenThanh-PC>
2018-04-24 20:35:00 +0700

    To Minh TUye- THuc Hanh voi mang tinh TB Cong

---
+++ Y2018JC34/src/com/imic/student/ToMinhTuyen/ThucHanhVoiMang.java
@@ -0,0 +1,30 @@
+package com.imic.student.ToMinhTuyen;
+
+import java.util.Scanner;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+public class ThucHanhVoiMang {
+
+	public static void main(String[] args) {
+		int soluong;
+		int tong = 0;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Moi Ban Nhap So Luong phan tu cua mang");
+		soluong = sc.nextInt();
+		int a[] = new int[soluong];
+		System.out.println("Nhap cac phan tu cua mang");
+		for (int i = 0; i < soluong; i++) {
+			System.out.println("Phan tu " + i);
+			a[i] = sc.nextInt();
+		}
+		for (int temp : a) {
+			tong = tong + temp;
+		}
+		double tb = tong / soluong;
+		System.out.println("Trung binh cong cac phan tu cua mang :" + tb);
+
+	}
+
+}
