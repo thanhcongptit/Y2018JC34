07ea947ab28112d4a041e106f450daf153901bfb
PC <PC@DESKTOP-IO7V3US>
2018-04-24 19:28:51 +0700

    abc

--- Y2018JC34/src/com/imic/students/nhatminh/Btv.java
+++ Y2018JC34/src/com/imic/students/nhatminh/Btv.java
@@ -6,26 +6,9 @@ public class Btv {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		String thang1;
-		String thang2;
-		String thang3;
-		String thang4;
-		String thang5;
-		String thang6;
-		String thang7;
-		String thang8;
-		String thang9;
-		String thang10;
-		String thang11;
-		String thang12;
-		System.out.println("Nhap thang");
-		String thang = sc.nextLine();
-		if (thang.equalsIgnoreCase("thang1") && thang.equalsIgnoreCase("thang4") && thang.equalsIgnoreCase("thang6")
-				&& thang.equalsIgnoreCase("thang9") && thang.equalsIgnoreCase("thang11")) {
-			System.out.println("Thang do co 30 ngay");
-		}
+		
+		
 
-	}
+	
 
-}
+
