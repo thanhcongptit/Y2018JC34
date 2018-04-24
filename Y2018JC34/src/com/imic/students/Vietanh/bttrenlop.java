dab482c7a8d8de64a55660a51544fdeb7d13c95b
DELL <DELL@DESKTOP-3QRE1GP>
2018-04-24 20:40:20 +0700

    viet anh: bt trung binh cong

--- Y2018JC34/src/com/imic/students/Vietanh/bttrenlop.java
+++ Y2018JC34/src/com/imic/students/Vietanh/bttrenlop.java
@@ -5,26 +5,24 @@ import java.util.Scanner;
 public class bttrenlop {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Xin moi ban nhap UserName");
-		String UserName = sc.nextLine();
-		System.out.println("Xin moi ban nhap Pass");
-		String Pass = sc.nextLine();
-
-		if (UserName.equals("")) {
-			System.out.println("Ban chua nhap UserName");
-		} else if (Pass.equals("")) {
-			System.out.println("Ban chua nhap Pass");
-		} else {
-			System.out.println("UserName cua ban: " + UserName);
-			System.out.println("Pass cua ban: " + Pass);
-			if (UserName.equals("abc") && Pass.equals("123456")) {
-				System.out.println("Ban da dang nhap thanh cong");
-
-			} else {
-				System.out.println("Dang nhap k thanh cong");
-			}
+		System.out.println("Xin moi ban nhap mang voi n so nguyen");
+		int n = sc.nextInt();
+		
+		int a[] = new int[n];
+		for(int i=0; i<n;i++) {
+			System.out.println("a" + i + "=");
+			a[i] = sc.nextInt();
+			
+			
 		}
-
+    double tong = 0;
+    for(int x:a) {
+    	tong = (tong+x);
+    	
+    	
+    }   
+    double tbc = tong/n;
+    	System.out.println("Trung binh cong: "+tbc);
 	}
 
 }
