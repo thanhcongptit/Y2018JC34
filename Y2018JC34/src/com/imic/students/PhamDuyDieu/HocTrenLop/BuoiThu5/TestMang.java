a52e644bbf2ee63fe54f42832887a3c8ab015c7d
Duy Diệu <Duy Diu@DESKTOP-THNIB20>
2018-04-24 20:25:44 +0700

    Pham Duy Dieu

--- Y2018JC34/src/com/imic/students/PhamDuyDieu/HocTrenLop/BuoiThu5/TestMang.java
+++ Y2018JC34/src/com/imic/students/PhamDuyDieu/HocTrenLop/BuoiThu5/TestMang.java
@@ -6,30 +6,26 @@ public class TestMang {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-/*
- * Khai báo mảng số nguyên: 
- * int[]a;
- * int b[];
- * String[]
- * BÀI TẬP: Nhập vào 1 mảng số nguyên. Tính và xuất trung bình cộng.
- */
-		int b=0;
-		double TBC=0;
+		/*
+		 * Khai báo mảng số nguyên: int[]a; int b[]; String[] BÀI TẬP: Nhập vào 1 mảng
+		 * số nguyên. Tính và xuất trung bình cộng.
+		 */
+		int b = 0;
+		double TBC = 0;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Mời bạn nhập vào số lượng phần tử của mảng: ");
 		b = sc.nextInt();
 		int a[] = new int[b];
-		System.out.println("Bạn vừa khai báo mảng a["+b+"]\nMời bạn nhập vào giá trị các phần tử: ");
-		for(int i=0; i<b; i++) {
-			System.out.print("a["+i+"] = ");
-			a[i]= sc.nextInt();
-			TBC +=a[i];
-			if(i==(b-1)) {
-				System.out.println("Trung bình cộng: "+ ((TBC)/b));
+		System.out.println("Bạn vừa khai báo mảng a[" + b + "]\nMời bạn nhập vào giá trị các phần tử: ");
+		for (int i = 0; i < b; i++) {
+			System.out.print("a[" + i + "] = ");
+			a[i] = sc.nextInt();
+			TBC += a[i];
+			if (i == (b - 1)) {
+				System.out.println("Trung bình cộng: " + ((TBC) / b));
 			}
 		}
-		
-		
+
 	}
 
 }
