94f165450a078e8b7a6d4f58f501a8ad61c18e81
hb2 <hb2@DESKTOP-39KL164>
2018-05-03 18:39:49 +0700

    vu viet duong

---
+++ Y2018JC34/src/com/imic/students/vuvietduongbaitap/buoi5/bai3.java
@@ -0,0 +1,10 @@
+package com.imic.students.vuvietduongbaitap.buoi5;
+
+public class bai3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Y2018JC34/src/com/imic/students/vuvietduongbaitap/buoi5/bai4.java
@@ -0,0 +1,10 @@
+package com.imic.students.vuvietduongbaitap.buoi5;
+
+public class bai4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
