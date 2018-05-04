4dd34f3414f559e83481610e9ab4fda874cf08ea
PC <PC@DESKTOP-IO7V3US>
2018-05-04 19:44:00 +0700

    nhatminh

--- Y2018JC34/src/com/imic/students/phamhoangnhatminh/Abc.java
+++ Y2018JC34/src/com/imic/students/phamhoangnhatminh/Abc.java
@@ -8,15 +8,17 @@ public class Abc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
+		int m = 0;
+    	int sohang = 0;
 		System.out.println("MOI BAN NHAP SO PHAN TU CUA MANG");
         int a = sc.nextInt();
         int b[]=new int[a];
         System.out.println("ban vua nhap mot mang b["+a+"]:");
         for(int i=0;i<a;i++) {
         	System.out.println("Nhap gia tri phan tu b["+i+"] = ");
-        	b[i] = sc.nextInt();
+        b[i] = sc.nextInt();
         	if(i==(a-1)) {
-        		if(i==(a-1)) {
+        		 {
             		Arrays.sort(b);
         		}
         		System.out.println("thu tu tang dan la");
@@ -25,14 +27,40 @@ public class Abc {
            		System.out.println(x);
            		continue;
            		}
+			
            		 int c = b [0];
            		 System.out.println("gia tri nho nhat la ");
            		 System.out.println(c);
-        	
-        		}
         	}
+         			}
+        	for (int temp : b) {
+     			if ((temp % 3) == 0) {
+     				m = m + temp;
+     				sohang++;
+		System.out.println("TBC cua cac phan tu % 3 :" + m / sohang);
+           		
+        	}
+        }
+	}
+}
+        	
+	
+      
+
+                 
+
+        	
+  				
+    						
+    						
+    			
+        
+	
+       	
+        	
+        	
         		
         	
-	}
-	}
+	
+	
 
