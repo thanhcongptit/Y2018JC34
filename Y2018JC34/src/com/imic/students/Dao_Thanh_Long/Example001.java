c94645a149bed08e1c0c85023e9d3a47c4d745a9
DaoThanhLong <30427088+DaoThanhLong@users.noreply.github.com>
2018-05-07 16:45:00 +0700

    btvn

--- Y2018JC34/src/com/imic/students/Dao_Thanh_Long/Example001.java
+++ Y2018JC34/src/com/imic/students/Dao_Thanh_Long/Example001.java
@@ -1,38 +1,58 @@
 package com.imic.students.Dao_Thanh_Long;
 
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
+
+import com.oracle.webservices.internal.impl.internalspi.encoding.StreamDecoder;
 
 public class Example001 {
 
-	public static void main(String[] args) {		
-		int soPhanTu=0;
-		double arr[];
-		double tong=0;
-		boolean tiepTuc = true;
+	public static void main(String[] args) {
+		String str;
+		String[] strArray1;
+		ArrayList<String> strArray2 = new ArrayList<String>();
+		ArrayList<String> strTemp1 = new ArrayList<String>();
 		Scanner objSc = new Scanner(System.in);
+		System.out.println("Nhap xau ki tu");
+		str = objSc.nextLine();
 		
-		while(tiepTuc){
-			try {
-				System.out.println("Nhap so phan tu cua mang a");
-				soPhanTu = objSc.nextInt();
-				tiepTuc = false;
-			} catch (Exception e) {
-				System.out.println("Gia tri ban nhap khong hop le");
-				objSc.nextLine();
+		strArray1 = str.split(" ");		
+		// mang cac tu
+		for(int i=0;i<strArray1.length;i++){
+			strTemp1.add(strArray1[i]);
+		}
+		
+		// xoa cac tu giong nhau
+		for(int i=0;i<strArray1.length-1;i++){
+			for(int j=i+1;j<strArray1.length;j++){
+				if(strArray1[i].equals(strArray1[j])){
+					strTemp1.set(j, "");
+				}
 			}
 		}
 		
-		arr = new double[soPhanTu];	
+		// Luu cac tu con lai sau khi xoa vao mang strArray2
+		for(int i=0;i<strTemp1.size();i++){
+			if(strTemp1.get(i).equals("")){
+				
+			}else{
+				strArray2.add(strTemp1.get(i));
+			}
+		}
 		
-		for(int i=0;i<soPhanTu;i++){
-			System.out.printf("a[%d] = ",i);
-			arr[i] = objSc.nextDouble();
-			tong += arr[i];
-		}	
-		
-		System.out.printf("Trung binh cong = %.2f" , (tong/soPhanTu));
-			
+		// Dem so lan xuat hien	
+		int dem;
+		for(int i=0;i<strArray2.size();i++){
+			dem = 0;
+			for(int j=0;j<strArray1.length;j++){
+				if(strArray2.get(i).equals(strArray1[j])){
+					dem+=1;
+				}
+			}
+			System.out.println(strArray2.get(i) + " xuat hien " + dem + " lan");
+		}
 		objSc.close();
 	}
 	
